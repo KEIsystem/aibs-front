9d04ca209b0273c90e51a9cbb9276b7597d87af5
Kei Ishii <keiishii.kyt2015@gmail.com>
2025-05-27 13:46:13 +0900

    DoubtCase url change

--- src/utils/saveDoubtCase.js
+++ src/utils/saveDoubtCase.js
@@ -1,14 +1,16 @@
 // src/utils/saveDoubtCase.js
 import axios from 'axios';
 
+const BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
 export const saveDoubtCase = async (caseType, inputData, outputData, comment) => {
   try {
-    await axios.post('/api/doubtcase/save_doubt_case/', {
-        case_type: caseType,
-        input_data: inputData,
-        output_data: outputData,
-        comment: comment || ""
-        });
+    await axios.post(`${BASE_URL}/api/doubtcase/save_doubt_case/`, {
+      case_type: caseType,
+      input_data: inputData,
+      output_data: outputData,
+      comment: comment || ""
+    });
     alert('✅ 疑問症例として保存しました');
   } catch (error) {
     console.error('❌ 保存失敗:', error);
